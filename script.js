4872f3156451150e3527d00a92373ac59dd04f54
Subham Kundu <subhamkundu.bankura12@gmail.com>
2023-10-04 02:36:52 +0530

    add your fav music and enjoy it

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const title = document.getElementById("title");
 const artist = document.getElementById("artist");
 const previous = document.getElementById("previous");
 const next = document.getElementById("next");
+let pogress = document.getElementById("pogress");
+let current_Time = document.getElementById("current-time");
+let tot_Duration = document.getElementById("duration");
+let pogressDiv = document.getElementById("pogress-div");
+
 
 const songs=[{
     img:"ashes",
@@ -69,5 +74,46 @@ const prevSong = ()=>{
    loading(songs[songIndex]);
    playMusic();
 }
+
+//pogress js work
+music.addEventListener("timeupdate" ,(event)=>{
+    // console.log(event);
+    const{currentTime,duration} = event.srcElement;
+    // console.log(currentTime);
+    // console.log(duration);
+    let pogressTime = ((currentTime/duration)*100);
+    pogress.style.width = `${pogressTime}%`;
+
+    //music duration update
+    let minDuration = Math.floor(duration/60);
+    let secDuration = Math.floor(duration%60);
+    if(duration){
+        tot_Duration.textContent = `${minDuration}:${secDuration}`;
+    }
+    
+    //music current time 
+    let currentMin = Math.floor(currentTime/60);
+    let currentSec = Math.floor(currentTime%60);
+    if(currentSec<10){
+        currentSec = `0${currentSec}`
+    }
+    current_Time.textContent = `${currentMin}:${currentSec}`;
+    
+});
+
+
+pogressDiv.addEventListener("click" , (event)=>{
+    console.log(event);
+    const{duration} = music;
+    // const duration = music.duration;
+    let movepogress = (event.offsetX/event.srcElement.clientWidth)*duration;
+    console.log(duration);
+    console.log(movepogress);
+    music.currentTime = movepogress;
+})
+
+//if music is end and call the next song
+music.addEventListener("ended",nextSong);
+
 next.addEventListener("click",nextSong);
 previous.addEventListener("click",prevSong);
